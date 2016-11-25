792a7d1804e19a86d6b9c4bc498843e15cb315c8
luojinghui <939035243@qq.com>
2016-11-25 20:48:18 +0800

    alte

--- index.ios.js
+++ index.ios.js
@@ -10,22 +10,67 @@ import {
     StyleSheet,
     Image,
     View,
-    Text
+    Text,
+    ScrollView,
+    TextInput
 } from 'react-native';
+
+// class Helloworld extends Component {
+//     render() {
+//         let pic = {
+//             uri: 'https://luojinghui.github.io/gallery-by-react/dist/assets/43af53fe5478f068b48b0fad9895e6f7.jpg'
+//         };
+//
+//         return (
+//             <View style={styles.container}>
+//                 <Image source={pic} style={styles.image}/>
+//                 <Greeting text="Rexxa"/>
+//                 <Greeting text="Jaina"/>
+//                 <Text style={ [styles.text, styles.text2 ]}>it's ok!</Text>
+//             </View>
+//         );
+//     }
+// }
 
 class Helloworld extends Component {
     render() {
         let pic = {
             uri: 'https://luojinghui.github.io/gallery-by-react/dist/assets/43af53fe5478f068b48b0fad9895e6f7.jpg'
         };
+        let message = {
+            uri: './src/image/Box.png'
+        }
 
         return (
-            <View style={styles.container}>
-                <Image source={pic} style={styles.image}/>
-                <Greeting text="Rexxa"/>
-                <Greeting text="Jaina"/>
-                <Text style={ [styles.text, styles.text2 ]}>it's ok!</Text>
-            </View>
+            <ScrollView style={{ flex: 1 }}>
+                <View style={{ flex: 1, height:  64, backgroundColor: '#ef4e24', flexDirection: 'row', paddingLeft: 10, alignItems: 'center',paddingTop: 20}}>
+                    <TextInput style={{ flex: 1,height: 36, backgroundColor: "#fff", borderRadius: 4,  fontSize: 14, paddingLeft: 40}} placeholder="搜索专家名称"/>
+                    <Image source={require('./src/image/Box.png')} style={{ height: 20, width: 20, marginLeft: 20, marginRight: 20, marginBottom: 8 }}/>
+                </View>
+                <View style={{ flex: 1, flexDirection: 'column' , paddingLeft: 10, paddingRight: 10}}>
+                    <View style={{ flexDirection: 'row', paddingTop: 10}}>
+                        <View style={{ flex: 1, marginRight: 10 , backgroundColor: 'powderblue' }}>
+                            <Image source={pic} style={{flex: 1, height: 222.5}}/>
+                            <Text>sadfasdf</Text>
+                        </View>
+                        <View style={{ flex: 1, backgroundColor: 'skyblue' }}>
+                            <Image source={pic} style={{flex: 1, height: 222.5}}/>
+                            <Text>123</Text>
+                        </View>
+                    </View>
+                    <View style={{  flexDirection: 'row', paddingTop: 10}}>
+                        <View style={{ flex: 1, marginRight: 10 , backgroundColor: 'powderblue' }}>
+                            <Image source={pic} style={{flex: 1, height: 222.5}}/>
+                            <Text>123</Text>
+                        </View>
+                        <View style={{ flex: 1, backgroundColor: 'skyblue' }}>
+                            <Image source={pic} style={{flex: 1, height: 222.5}}/>
+                            <Text>123</Text>
+                        </View>
+                    </View>
+                </View>
+                <Text>123123</Text>
+            </ScrollView>
         );
     }
 }
